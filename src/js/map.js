7b45950edc9c96637fc3c3c7e534ac3beb86648a
byanofsky <byanofsky@me.com>
2017-05-09 13:56:54 -0700

    Add custom styling to map

--- src/js/map.js
+++ src/js/map.js
@@ -1,8 +1,134 @@
 function initMap() {
+  // Map style from https://snazzymaps.com/style/27/shift-worker
+  var styledMapType = new google.maps.StyledMapType(
+    [
+      {
+        "stylers": [
+          {
+            "saturation": -100
+          },
+          {
+            "gamma": 1
+          }
+        ]
+      },
+      {
+        "elementType": "labels.text.stroke",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.business",
+        "elementType": "labels.text",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.business",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.place_of_worship",
+        "elementType": "labels.text",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.place_of_worship",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "geometry",
+        "stylers": [
+          {
+            "visibility": "simplified"
+          }
+        ]
+      },
+      {
+        "featureType": "water",
+        "stylers": [
+          {
+            "visibility": "on"
+          },
+          {
+            "saturation": 50
+          },
+          {
+            "gamma": 0
+          },
+          {
+            "hue": "#50a5d1"
+          }
+        ]
+      },
+      {
+        "featureType": "administrative.neighborhood",
+        "elementType": "labels.text.fill",
+        "stylers": [
+          {
+            "color": "#333333"
+          }
+        ]
+      },
+      {
+        "featureType": "road.local",
+        "elementType": "labels.text",
+        "stylers": [
+          {
+            "weight": 0.5
+          },
+          {
+            "color": "#333333"
+          }
+        ]
+      },
+      {
+        "featureType": "transit.station",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "gamma": 1
+          },
+          {
+            "saturation": 50
+          }
+        ]
+      }
+    ], {name: 'Styled Map'});
+
   // Create a map object and specify the DOM element for display.
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 33.972536, lng: -118.426561},
     scrollwheel: false,
-    zoom: 14
+    zoom: 14,
+    mapTypeControlOptions: {
+        mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+                     'styled_map']
+      }
   });
+
+  // Associate the styled map with the MapTypeId and set it to display.
+  map.mapTypes.set('styled_map', styledMapType);
+  map.setMapTypeId('styled_map');
 }
