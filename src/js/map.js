5104ef84dc4334447ed4f6a125a7357a8bd9ae74
byanofsky <byanofsky@me.com>
2017-05-09 15:15:16 -0700

    Add geocode function to get position of address

--- src/js/map.js
+++ src/js/map.js
@@ -1,4 +1,45 @@
+var locations = [
+  {
+    name: 'Test Broken',
+    address: '1212 South Fake Street, Los Angeles, CA 90094'
+  },
+  {
+    name: 'YouTube Spaces',
+    address: '12422 Bluff Creek Dr, Los Angeles, CA 90094'
+  },
+  {
+    name: 'Fullscreen, Inc.',
+    address: '12180 Millennium Dr, Los Angeles, CA 90094'
+  },
+  {
+    name: 'IMAX Post/DKP Inc.',
+    address: '12582 Millennium Dr, Playa Vista, CA 90094'
+  },
+  {
+    name: 'Belkin International',
+    address: '12045 Waterfront Dr, Los Angeles, CA 90094'
+  },
+  {
+    name: 'Yahoo Inc, Playa Vista. Office',
+    address: '11975 Bluff Creek Dr, Los Angeles, CA 90094'
+  },
+  {
+    name: 'Electronic Arts (EA)',
+    address: '5510 Lincoln Blvd, Playa Vista, CA 90094'
+  },
+  {
+    name: 'ICANN',
+    address: '12025 E Waterfront Dr #300, Los Angeles, CA 90094'
+  }
+];
+
 function initMap() {
+  // Store all markers in this array
+  var markers = [];
+
+  // Initialize geocoding service
+  var geocoder = new google.maps.Geocoder();
+
   // Map style from https://snazzymaps.com/style/27/shift-worker
   var styledMapType = new google.maps.StyledMapType(
     [
@@ -127,14 +168,36 @@ function initMap() {
       }
   });
 
-  var marker = new google.maps.Marker({
-    position: {lat: 33.976977, lng: -118.407325},
-    map: map,
-    title: 'Hello World!',
-    animation: google.maps.Animation.DROP
-  });
-
   // Associate the styled map with the MapTypeId and set it to display.
   map.mapTypes.set('styled_map', styledMapType);
   map.setMapTypeId('styled_map');
+
+  // Cycle through location addresses and create markers on map
+  for (var i = 0; i < locations.length; i++) {
+    geocodeAddressToMarker(locations[i], map, geocoder, markers);
+  }
+}
+
+// Turns a location into a location using geocode service, and creates marker
+function geocodeAddressToMarker(location, map, geocoder, markers) {
+  geocoder.geocode({
+    address: location.address
+  }, function(results, status) {
+    if (status === 'OK') {
+      markers.push(createMarker(results[0].geometry.location, map, location.name));
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+
+// Creates a default marker for map
+function createMarker(position, map, title) {
+  var marker = new google.maps.Marker({
+    map: map,
+    position: position,
+    title: title,
+    animation: google.maps.Animation.DROP
+  });
+  return marker;
 }
