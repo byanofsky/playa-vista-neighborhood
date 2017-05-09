2ec854ea2053301c794c2d8aad577e4c1a52cb90
byanofsky <byanofsky@me.com>
2017-05-09 15:34:45 -0700

    Add initial knockout list functionality

---
+++ src/js/app.js
@@ -0,0 +1,36 @@
+var locations = [
+  {
+    name: 'YouTube Spaces',
+    address: '12422 Bluff Creek Dr, Los Angeles, CA 90094'
+  },
+  {
+    name: 'Fullscreen, Inc.',
+    address: '12180 Millennium Dr, Los Angeles, CA 90094'
+  },
+  {
+    name: 'IMAX Post/DKP Inc.',
+    address: '12582 Millennium Dr, Playa Vista, CA 90094'
+  },
+  {
+    name: 'Belkin International',
+    address: '12045 Waterfront Dr, Los Angeles, CA 90094'
+  },
+  {
+    name: 'Yahoo Inc, Playa Vista. Office',
+    address: '11975 Bluff Creek Dr, Los Angeles, CA 90094'
+  },
+  {
+    name: 'Electronic Arts (EA)',
+    address: '5510 Lincoln Blvd, Playa Vista, CA 90094'
+  },
+  {
+    name: 'ICANN',
+    address: '12025 E Waterfront Dr #300, Los Angeles, CA 90094'
+  }
+];
+
+var ViewModel = function(locations) {
+  this.locations = ko.observableArray(locations);
+};
+
+ko.applyBindings(new ViewModel(locations));
