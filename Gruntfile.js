dfe6f687b344566b3e8bb9fb6fe9d40881608528
byanofsky <byanofsky@me.com>
2017-05-09 10:10:41 -0700

    Add watch and copy to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,11 +4,34 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      build: ['Gruntfile.js', 'src/**/*.js']
+    },
+    watch: {
+      scripts: {
+        files: 'src/**/*.js',
+        tasks: ['jshint']
+      }
+    },
+    copy: {
+      build: {
+        files: [
+          {
+            expand: true,
+            src: 'node_modules/knockout/build/output/knockout-latest.js',
+            dest: 'dist/js/',
+            flatten: true,
+            rename: function (dest, src) {
+              return dest + 'knockout.js';
+            }
+          }
+        ]
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task(s).
   grunt.registerTask('default', ['jshint']);
