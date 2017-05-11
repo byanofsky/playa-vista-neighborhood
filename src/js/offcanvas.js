912ca6d23b03f8515b19ccbb6969495800812de0
byanofsky <byanofsky@me.com>
2017-05-11 13:28:15 -0700

    Add offcanvas menu show by default when window >= 768

--- src/js/offcanvas.js
+++ src/js/offcanvas.js
@@ -1,7 +1,15 @@
-// from http://getbootstrap.com/examples/offcanvas/
 $(document).ready(function () {
-  $('[data-toggle="offcanvas"]').click(function () {
+  // TODO: this can be moved to viewmodel
+  // Show offcanvas menu by default when window >= 768
+  if ($(window).width() >= 768) {
+    toggleOffcanvas();
+  }
+  // Toggle the offcanvas menu
+  $('[data-toggle="offcanvas"]').click(toggleOffcanvas);
+  // Display offcanvas menu
+  // from http://getbootstrap.com/examples/offcanvas/
+  function toggleOffcanvas() {
     $('.row-offcanvas').toggleClass('active');
     $('#map-container').toggleClass('col-xs-9');
-  });
+  }
 });
