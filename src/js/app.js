2f378c0fc1d1a18a75eae3ef40139872ea9ebc66
byanofsky <byanofsky@me.com>
2017-05-12 14:47:26 -0700

    Add basic test search with yelp api call

--- src/js/app.js
+++ src/js/app.js
@@ -27,9 +27,39 @@ var ViewModel = function() {
   self.clearLocations = function() {
     self.locations.removeAll();
   };
+  // Remove all locations and map markers
+  self.clearLocationsAndMarkers = function() {
+    self.clearLocations();
+    clearMarkers();
+  };
   // Perform a new search for locations
   self.newSearch = function(type) {
     getPlaceSearch(type.value);
+  };
+  // Perform yelp restaurant search
+  self.yelpSearch = function(search, location) {
+    var url = "http://localhost:5000/";
+    $.getJSON( url, {
+      search: 'restaurants',
+      location: 'playa vista'
+    }, function( data ) {
+      console.log(data);
+      self.clearLocationsAndMarkers();
+      for (var i = 0; i < data.businesses.length; i++) {
+        var businessData = data.businesses[i];
+        var business = {
+          name: businessData.name,
+          position: {
+            lat: businessData.coordinates.latitude,
+            lng: businessData.coordinates.longitude
+          }
+        };
+        self.locations.push(business);
+        var marker = createMarker(business.position, business.name);
+        markers.push(marker);
+      }
+      setMarkersMap(map);
+    });
   };
 };
 
