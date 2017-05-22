9e84155ae15a4a32930c1354c11d075a6daf3824
byanofsky <byanofsky@me.com>
2017-05-22 13:54:35 -0700

    Move offcanvas jquery functions to within viewmodel

--- src/js/app.js
+++ src/js/app.js
@@ -3,40 +3,6 @@ var locationCenter = {lat: 33.9739136, lng: -118.4161883};
 // Set backend server url to make calls to yelp api
 var yelpApiUrl = "http://localhost:5000/";
 
-$(document).ready(function () {
-  // Show offcanvas menu by default when window >= 768
-  if ($(window).width() >= 768) {
-    toggleOffcanvas();
-  }
-  // Set the main content height to match screen size
-  setMainContentHeight();
-  // TODO: this can be moved to viewmodel
-  // Offcanvas menu from http://getbootstrap.com/examples/offcanvas/
-  // Toggle the offcanvas menu
-  $('[data-toggle="offcanvas"]').click(toggleOffcanvas);
-  // Adjust main content height when window resizes
-  $( window ).resize(setMainContentHeight);
-  // Adjust main content height when toggling collapse menu
-  // $('#main-nav').on('show.bs.collapse hide.bs.collapse', setMainContentHeightCollapsing);
-});
-
-function toggleOffcanvas() {
-  $('.row-offcanvas').toggleClass('active');
-  $('#map-container').toggleClass('col-xs-6 col-sm-9');
-  // Check if Google maps API has loaded
-  if (typeof google !== "undefined" && typeof google.maps !== "undefined") {
-    // Trigger resize for map since #map-container changes size
-    google.maps.event.trigger(map, 'resize');
-  }
-}
-
-// Change height of main content to keep app full height
-function setMainContentHeight() {
-  var windowHeight = $(window).height();
-  var navbarHeight = $('.navbar').height();
-  $('#main-content').height(windowHeight-navbarHeight);
-}
-
 var ViewModel = function() {
   var self = this;
   // Track restaurant data
@@ -186,8 +152,37 @@ var ViewModel = function() {
   self.getYelpStarIMG = function(rating) {
     return 'small_' + String(rating).replace('.5', '_half');
   };
+  // Toggle offcanvas restaurant list
+  self.toggleOffcanvas = function() {
+    // Offcanvas menu from http://getbootstrap.com/examples/offcanvas/
+    $('.row-offcanvas').toggleClass('active');
+    $('#map-container').toggleClass('col-xs-6 col-sm-9');
+    // Check if Google maps API has loaded
+    if (typeof google !== "undefined" && typeof google.maps !== "undefined") {
+      // Trigger resize for map since #map-container changes size
+      google.maps.event.trigger(map, 'resize');
+    }
+  };
 };
 
 var viewModelInstance = new ViewModel();
 
 ko.applyBindings(viewModelInstance);
+
+$(document).ready(function () {
+  // Show offcanvas menu by default when window >= 768
+  if ($(window).width() >= 768) {
+    viewModelInstance.toggleOffcanvas();
+  }
+  // Set the main content height to match screen size
+  setMainContentHeight();
+  // Adjust main content height when window resizes
+  $( window ).resize(setMainContentHeight);
+
+  // Change height of main content to keep app full height
+  function setMainContentHeight() {
+    var windowHeight = $(window).height();
+    var navbarHeight = $('.navbar').height();
+    $('#main-content').height(windowHeight-navbarHeight);
+  }
+});
