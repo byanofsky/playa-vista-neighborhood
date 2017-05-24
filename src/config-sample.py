6e535a2d597f172d9342fb8a547335890c474b49
byanofsky <byanofsky@me.com>
2017-05-24 12:08:20 -0700

    Add a sample config file

---
+++ src/config-sample.py
@@ -0,0 +1,6 @@
+FLASK_SECRET_KEY = 'Enter a Flask Secret Key'
+
+# OAuth Credentials. You can find them on
+# https://www.yelp.com/developers/v3/manage_app
+YELP_CLIENT_ID = 'Enter Yelp Client ID'
+YELP_CLIENT_SECRET = 'Enter Yelp Client Secret'
